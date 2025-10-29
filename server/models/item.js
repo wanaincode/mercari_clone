a4bd22df4734ab563894be40fada95152a5caf31
wanaincode <91902459+wanaincode@users.noreply.github.com>
2025-10-30 00:59:50 +0900

    fix: add sold field to Item type

--- server/models/item.js
+++ server/models/item.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
-const itemSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  price: { type: Number, required: true },
-  imageUrl: { type: String },
-  seller: { type: String, required: true },
-}, { timestamps: true });
+const itemSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    imageUrl: { type: String },
+    seller: { type: String, required: true },
+    sold: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 const Item = mongoose.model("Item", itemSchema);
 export default Item;
